267f64aaf5bef9a08244daf1c5a8172fc7d1dcac
dundarkarabay <53024047+dundarkarabay@users.noreply.github.com>
2019-10-31 10:19:15 -0500

    Pushing the level-2 part

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,22 @@
+// YOUR CODE HERE!
+const submit = d3.select("button#filter-btn");
+
+function ufoSightingFilter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    d3.selectAll("tbody>tr").remove()
+    var inputValue = d3.select("#sub-category").property("value");
+    var inputData = data.filter(d => (d.datetime===inputValue || d.city===inputValue || d.state===inputValue || d.country===inputValue || d.shape===inputValue));
+    inputData.forEach(d => {
+        let new_tr = d3.select("tbody").append("tr");
+        new_tr.append("td").text(d.datetime);
+        new_tr.append("td").text(d.city);
+        new_tr.append("td").text(d.state);
+        new_tr.append("td").text(d.country);
+        new_tr.append("td").text(d.shape);
+        new_tr.append("td").text(d.durationMinutes);
+        new_tr.append("td").text(d.comments);
+    })
+}
+
+submit.on("click", ufoSightingFilter);
